456e0f5f1cc5f8da6723cd61e5af6caa4c6ab7de
ShaniSapir <94107440+ShaniSapir@users.noreply.github.com>
2021-12-01 15:57:39 +0200

    remove x from sort A

--- Source.c
+++ Source.c
@@ -2570,13 +2570,11 @@ void sort_A(char category[5])
 	scanf(" %s", choice2);
 	if (atoi(choice2) > 0 || atoi(choice2) <= atoi(category))
 	{ ///////////////////////////////////////////////////////check stock//////////////////////////////////////
-		printf("What is the quantity you want? Press x and then the new amount.\n");
-		scanf(" %c", &d);
+		printf("What is the quantity you want? \n");
 		scanf(" %s", quantity);
-		while (d != 'x' || d != 'X' || check_number(quantity) == 1 || atoi(quantity) > atoi(test->amount_of_product) || 0 < atoi(test->amount_of_product))
+		while (check_number(quantity) == 1 || atoi(quantity) > atoi(test->amount_of_product) || 0 < atoi(test->amount_of_product))
 		{
 			printf("Your input is not valid, Try again.\n");
-			scanf(" %c", &d);
 			scanf(" %s", quantity);
 		}
 		FILE* fic2 = fopen("Basket.csv", "r+");
